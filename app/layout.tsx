de79d525943265996d4372ce98f08b34664e890f
ulrich <koudjouulrich@gmail.com>
2026-06-09 00:53:29 +0100

    add-versel analytics-1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -22,6 +23,7 @@ export default function RootLayout({
       </head>
       <body className="bg-background text-on-surface font-body-md overflow-x-hidden">
         {children}
+        <Analytics />
       </body>
     </html>
   );
